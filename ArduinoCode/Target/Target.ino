bc2786d4bbdc00a4ea295467b140456b7b2031e2
SkHCrusher <tobiasstewen@cheater2.de>
2023-07-16 12:29:59 +0200

    Last Update from v1

--- ArduinoCode/Target/Target.ino
+++ ArduinoCode/Target/Target.ino
@@ -9,15 +9,20 @@
 #include <WiFiManager.h>
 WiFiManager wm;
 int cpTimeout = 90; // Timeout for the configuration portal
-WiFiManagerParameter custom_led_delay("led_delay", "LED delay", "", 10);
+char hit_duration[6] = "7000";
+char switch_duration[6] = "1000";
+char advertise_time[6] = "60000";
+char demo_mode[2] = "1";
+char target_name[30] = "InfinitagTarget_4";
+WiFiManagerParameter custom_hit_duration("hit_duration", "Hit duration", hit_duration, 6);
+WiFiManagerParameter custom_switch_duration("switch_duration", "Switch duration", switch_duration, 6);
+WiFiManagerParameter custom_advertise_time("advertise_time", "Advertise time", advertise_time, 6);
+WiFiManagerParameter custom_demo_mode("demo_mode", "Demo mode", demo_mode, 2);
+WiFiManagerParameter custom_target_name("target_name", "Name", target_name, 30);
 
 // Core
 #include <Infinitag_Core.h>
 Infinitag_Core infinitagCore;
-
-// WifiManager configuration portal button
-#define BTN_CP_START 17
-int buttonState = 0;
 
 // IR
 #define DECODE_RC5
@@ -45,10 +50,24 @@ int ledColorWifiWait[4] = {255, 255, 0, 0};
 int ledColorIdle[4] = {255, 0, 255, 0};
 int ledColorHit[4] = {255, 0, 0, 0};
 int ledColorMissHit[4] = {0, 255, 255, 0};
+int ledColorDemo[4] = {0, 20, 20, 0};
 
 // General
 bool targetHit = false;
+int hitDuration = 1000;
+float hitStep = 1000;
+bool demoMode = false;
+unsigned long lastHit;
 #define TEST_LED_PIN 2
+int advertiseTime = 10000;
+int switchDuration = 1000;
+
+
+//callback notifying us of the need to save config
+void saveConfigCallback () {
+  // Get saved vars
+  loadSavedVars();
+}
 
 void setup() {
   // Disable Log to prevent Serial Log
@@ -63,9 +82,6 @@ void setup() {
   // Wifi - 1
   WiFi.mode(WIFI_STA);
 
-  // Set configuration portal PIN
-  pinMode(BTN_CP_START, INPUT);
-
   // Set Switch Pins
   pinMode(SW_1, OUTPUT);
   pinMode(SW_2, OUTPUT);
@@ -73,17 +89,19 @@ void setup() {
   pinMode(SW_4, OUTPUT);
 
   // Custom configuration
-  wm.addParameter(&custom_led_delay);
-
-  // Set the default value for the custom field
-  custom_led_delay.setValue("1000",4);
+  wm.setSaveConfigCallback(saveConfigCallback);
+  wm.addParameter(&custom_hit_duration);
+  wm.addParameter(&custom_demo_mode);
+  wm.addParameter(&custom_target_name);
+  wm.addParameter(&custom_advertise_time);
+  wm.addParameter(&custom_switch_duration);
 
   // Custom menu
   // Added param for custom configuration
   // Added exit to start loop after configuration
   std::vector<const char *> menu = {"wifi","info","param","close","sep","update","exit"}; // Added param to the menu
   wm.setMenu(menu); // custom menu, pass vector
-  wm.setHostname("InfinitagTarget_1");
+  wm.setHostname(custom_target_name.getValue());
   
   // LED
   strip.begin();
@@ -94,21 +112,24 @@ void setup() {
   // Wifi - 2
   //wm.resetSettings(); // Only for Develop - Remove after
   // Auto Connect
-  if (!wm.autoConnect("InfinitagTarget_1","12345678")) {
+  if (!wm.autoConnect(custom_target_name.getValue(), "12345678")) {
   }
     
   // IR
   irrecv.enableIRIn();
 
+  // Get saved vars
+  loadSavedVars();
+  
   // General 
-  targetHit = false;
+  hitStep = hitDuration / LED_COUNT;
+  resetHit();
 }
 
 void loop() {
-  irDecode();
-
-  configPortalButton();
   
+  irDecode();
+    
   if (targetHit) {
     targetHitLoop();
   } else {
@@ -116,19 +137,65 @@ void loop() {
   }
 }
 
+void loadSavedVars() {
+  hitDuration = atoi(custom_hit_duration.getValue());
+  demoMode = (atoi(custom_demo_mode.getValue()) == 1) ? true : false;
+  advertiseTime = atoi(custom_advertise_time.getValue());
+  switchDuration = atoi(custom_switch_duration.getValue());
+}
+
+void resetHit() {
+  targetHit = false;
+  lastHit = millis();
+  IrReceiver.resume();
+}
+
+void setHit() {
+  targetHit = true;
+  lastHit = millis();
+  setAllSwitches(HIGH);
+  ledFillFull(ledColorHit);
+}
+
 void targetNotHitLoop() {
   setAllSwitches(LOW);
-  ledFillFull(ledColorIdle);
+  
+  if (demoMode) {
+    strip.clear();
+    strip.setPixelColor(0, strip.Color(ledColorDemo[0], ledColorDemo[1], ledColorDemo[2], ledColorDemo[3]));
+    strip.show();
+
+    if ((lastHit + advertiseTime) < millis()) {
+      setHit();
+    }
+  } else {
+    ledFillFull(ledColorIdle);
+  }
 }
 
 void targetHitLoop() {
-  setAllSwitches(HIGH);
-  
-  ledFillFull(ledColorHit);
-  
-  targetHit = false;
-  delay(1000);
-  IrReceiver.resume();
+  int diff = millis() - lastHit;
+  if (diff > hitDuration) {
+    resetHit();
+    return;
+  }
+
+  if ((lastHit + switchDuration) < millis()) {
+    setAllSwitches(LOW);
+  }
+
+  for (int i = 0; i < LED_COUNT; i++) {
+    int currentIndex = (diff / hitStep);
+    if (i < currentIndex) {
+      strip.setPixelColor(i, strip.Color(255, 0, 0, 0));
+    } else if (i == currentIndex) {
+      int redValue = (diff - (i * hitStep)) * 255 / hitStep;
+      strip.setPixelColor(i, strip.Color(redValue, 0, 0, 0));
+    } else {
+      strip.setPixelColor(i, strip.Color(0, 0, 0, 0));
+    }
+  }
+  strip.show();
 }
 
 void setAllSwitches(int state) {
@@ -136,16 +203,6 @@ void setAllSwitches(int state) {
   digitalWrite(SW_2, state);
   digitalWrite(SW_3, state);
   digitalWrite(SW_4, state);
-}
-
-void configPortalButton() {
-  buttonState = digitalRead(BTN_CP_START);
-
-  if (buttonState == HIGH) {
-    ledShowSetup();
-    wm.setConfigPortalTimeout(cpTimeout);
-    wm.startConfigPortal("WM_ConnectAP","12345678");
-  }
 }
 
 void irDecode() {
@@ -157,14 +214,24 @@ void irDecode() {
       
       // Validate all required Infinitag parameters for a valid hit
       if (infinitagCore.irRecvIsSystem == false && infinitagCore.irRecvGameId == 0
-        && infinitagCore.irRecvTeamId == 0 && infinitagCore.irRecvPlayerId == 0
-        && infinitagCore.irRecvCmd == 1 && infinitagCore.irRecvCmdValue == 255) {
-          targetHit = true;
+      && infinitagCore.irRecvTeamId == 0 && infinitagCore.irRecvPlayerId == 0
+      && infinitagCore.irRecvCmd == 1 && infinitagCore.irRecvCmdValue == 255) {
+        if (!targetHit) {
+          setHit();
+        }
+      } else if (infinitagCore.irRecvIsSystem == false && infinitagCore.irRecvGameId == 0
+      && infinitagCore.irRecvTeamId == 0 && infinitagCore.irRecvPlayerId == 0
+      && infinitagCore.irRecvCmd == 2 && infinitagCore.irRecvCmdValue == 255){
+        ledShowSetup();
+        wm.setConfigPortalTimeout(cpTimeout);
+        wm.startConfigPortal(custom_target_name.getValue(), "12345678");
       }
     } else {
-      ledFillFull(ledColorMissHit);
-      delay(150);
-      strip.clear();
+      if (!targetHit) {
+        ledFillFull(ledColorMissHit);
+        delay(150);
+        strip.clear();
+      }
     }
     
     IrReceiver.resume();
